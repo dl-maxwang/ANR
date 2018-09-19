1ac3bbeaadc62c98dcaac56a3cbc66bfba234d52
Max Wang <wangzhen.ace@foxmail.com>
2018-09-19 11:39:09 +0800

    Update ANRWatchDog.java

--- ANR/ANRWatchDog.java
+++ ANR/ANRWatchDog.java
@@ -7,7 +7,7 @@ public class ANRWatchDog implements Runnable {
 
     public static final int MESSAGE_WATCHDOG_TIME_TICK = 0;
     /**
-     * 判定Activity发生了ANR的时间，必须要小于5秒，否则等弹出ANR，可能就被用户立即杀死了。
+     * ANR time Threshhold
      */
     public static final int ACTIVITY_ANR_TIMEOUT = 2000;
 
@@ -33,7 +33,7 @@ public class ANRWatchDog implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //如果相等，说明过了ACTIVITY_ANR_TIMEOUT的时间后watchDogHandler仍没有处理消息，已经ANR了
+            // ANR happens
             if (timeTick == lastTimeTick) {
                 throw new ANRException();
             } else {
