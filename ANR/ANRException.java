ab83020f64d4e460896c9ebd4830ba9ceb0023bc
Max Wang <wangzhen.ace@foxmail.com>
2018-09-19 10:29:35 +0800

    init commit

---
+++ ANR/ANRException.java
@@ -0,0 +1,12 @@
+package com.zihuiinfo.facerecogntionsdk.example.ANR;
+
+import android.os.Looper;
+
+public class ANRException extends RuntimeException {
+    public ANRException() {
+        super("ANR happened");
+        Thread mainThread = Looper.getMainLooper().getThread();
+        setStackTrace(mainThread.getStackTrace());
+    }
+
+}
